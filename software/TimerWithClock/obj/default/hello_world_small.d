02b2be217f3d7bc7f6b79cbd1b639d57e80cbbca
Nicol Otárola <niicoporras@gmail.com>
2024-08-24 21:40:34 -0600

    Turn off leds after 30 seconds

---
+++ software/TimerWithClock/obj/default/hello_world_small.d
@@ -0,0 +1,15 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ c:/Users/USUARIO/Downloads/operativos/FPGA-Microarquitectura-Reloj-Despertador/software/TimerWithClock_bsp/system.h \
+ c:/Users/USUARIO/Downloads/operativos/FPGA-Microarquitectura-Reloj-Despertador/software/TimerWithClock_bsp/linker.h \
+ c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\hal\inc\sys\alt_stdio.h \
+ c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\hal\inc\io.h \
+ c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\hal\inc\alt_types.h \
+ c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\drivers\inc\altera_avalon_timer_regs.h
+c:/Users/USUARIO/Downloads/operativos/FPGA-Microarquitectura-Reloj-Despertador/software/TimerWithClock_bsp/system.h:
+c:/Users/USUARIO/Downloads/operativos/FPGA-Microarquitectura-Reloj-Despertador/software/TimerWithClock_bsp/linker.h:
+c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\hal\inc\sys\alt_stdio.h:
+c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\drivers\inc\altera_avalon_pio_regs.h:
+c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\hal\inc\io.h:
+c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\hal\inc\alt_types.h:
+c:\users\usuario\downloads\operativos\fpga-microarquitectura-reloj-despertador\software\timerwithclock_bsp\drivers\inc\altera_avalon_timer_regs.h:
